b4c767842b00f5ec885b69cb5e0b015701873cbb
SamuelLucca1002 <ls.webdesigner0201@gmail.com>
2022-09-27 15:50:11 -0300

    exercicio 1

--- exercicio 01/script.js
+++ exercicio 01/script.js
@@ -1,1 +1,23 @@
-alert("olá!!")
+function carregar() {
+
+var msg = document.getElementById('msg')
+var Image = document.getElementById('imagem')
+
+var data = new Date()
+var hora = data.getHours()
+var minuto = data.getMinutes()
+//var hora = 22
+msg.innerHTML = `Agora são ${hora}:${minuto}  horas`
+
+if (hora >= 5 && hora < 12 ) {
+    Image.src = 'imagens/manha.jpg'
+    document.body.style.background = '#cf7c38'
+} else if (hora > 12 && hora < 18) {
+    Image.src = 'imagens/tarde.jpg'
+    document.body.style.background = '#be2e00'
+} else {
+    Image.src = 'imagens/noite.jpg'
+    document.body.style.background = '#1e4f7a'
+}
+
+}
