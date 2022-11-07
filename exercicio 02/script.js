6e551645a5b4516f3d22b09b302b857a2a9dfca7
SamuelLucca1002 <ls.webdesigner0201@gmail.com>
2022-11-07 13:04:33 -0300

    novo site

--- exercicio 02/script.js
+++ exercicio 02/script.js
@@ -1,23 +1,60 @@
-function carregar() {
+//Verificador de idade
+function verificar() {
+    //Variaveis
+   var data = new Date();
+   var ano = data.getFullYear();
+   var fAno = document.getElementById("txtano");
+   var res = document.getElementById("res");
 
-var msg = document.getElementById('msg')
-var Image = document.getElementById('imagem')
+   if(fAno.value.length == 0 || fAno.value > ano) {
+    window.alert("Verifique se as datas estão corretas e tente novamente");
+   } else {
+        var fsex = document.getElementsByName("radsex");
+        var idade = ano - Number(fAno.value)
+        var genero = " "
+        var img = document.createElement('img');
+        img.setAttribute('id', 'foto')
 
-var data = new Date()
-var hora = data.getHours()
-var minuto = data.getMinutes()
-//var hora = 22
-msg.innerHTML = `Agora são ${hora}:${minuto}  horas`
+        if(fsex[0].checked) {
+            genero = "Homem"
 
-if (hora >= 5 && hora < 12 ) {
-    Image.src = 'imagens/manha.jpg'
-    document.body.style.background = '#cf7c38'
-} else if (hora > 12 && hora < 18) {
-    Image.src = 'imagens/tarde.jpg'
-    document.body.style.background = '#be2e00'
-} else {
-    Image.src = 'imagens/noite.jpg'
-    document.body.style.background = '#1e4f7a'
+            //Idade homem
+            if(idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'imagens/masculino/foto-bebe-m.png')
+            } else if(idade > 10 && idade <= 20) {
+                //jovem
+                img.setAttribute('src', 'imagens/masculino/foto-jovem-m.png')
+            } else if(idade > 20 && idade <= 50) {
+                //adulto
+                img.setAttribute('src', 'imagens/masculino/foto-adulto-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/masculino/foto-idoso-m.png')
+            }
+
+        } else if(fsex[1].checked) {
+            genero = "Mulher"
+
+            //idade mulher
+            if(idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'imagens/feminino/foto-bebe-f.png')
+            } else if(idade > 10 && idade <= 20) {
+                //jovem
+                img.setAttribute('src', 'imagens/feminino/foto-jovem-f.png')
+            } else if(idade > 20 && idade <= 50) {
+                //adulta
+                img.setAttribute('src', 'imagens/feminino/foto-adulto-f.png')
+            } else {
+                //idosa
+                img.setAttribute('src', 'imagens/feminino/foto-idoso-f.png')
+            }
+
+        }
+        res.style.textAlign = "center";
+        res.innerHTML = `Foi detectado ${genero} com ${idade} anos.`;
+        res.appendChild(img);
+   }
 }
-
-}
+//fim do verificador
